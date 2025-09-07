aceb0c5602e1a5ac0c503feb824629d65a7b5dc2
panasaraviteja <162182973+panasaraviteja@users.noreply.github.com>
2025-09-08 00:09:34 +0530

    Update EditForm.tsx

--- app/posts/[id]/edit/EditForm.tsx
+++ app/posts/[id]/edit/EditForm.tsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -79,4 +79,4 @@ export default function EditForm({ post }: { post: any }) {
       </div>
     </form>
   );
-}
+}./
